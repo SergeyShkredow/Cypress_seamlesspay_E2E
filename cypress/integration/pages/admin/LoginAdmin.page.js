1e690f717c0855325ecad557c390bb063d80c936
SergeyShkredow <1978tarf@gmail.com>
2020-12-06 20:31:15 +0200

    the logic of the login page has been updated

--- cypress/integration/pages/admin/LoginAdmin.page.js
+++ cypress/integration/pages/admin/LoginAdmin.page.js
@@ -1,26 +1,22 @@
-import {urlAdmin} from '../../../support/constants'
+import {urlAdmin} from '../../../support/constants';
 
 export class LoginAdminPage {
 
     goToLogin(siteDomain) {
         switch (siteDomain) {
             case 'rc-':
+            case 'test-':
                 cy.visit(`rc-${urlAdmin}`)
                 break;
             case 'prod':
                 cy.visit(`${urlAdmin}`)
-                break;
-            case 'test':
-                cy.visit(`test-${urlAdmin}`)
-                break;
-            default:
-                alert('We hope that this page looks ok!');
+                break
         }
     };
 
     signIn() {
-        cy.get('[type="email"]').type('seamlesspaytest3@gmail.com')
-        cy.get('[type="password"]').type('123123!')
-        cy.get('[type="button"]').click()
-    }
+        cy.get('[type="email"]').type('seamlesspaytest3@gmail.com');
+        cy.get('[type="password"]').type('123123!');
+        cy.get('[type="button"]').click();
+    };
 }
--- cypress/integration/pages/portal/LoginPortal.page.js
+++ cypress/integration/pages/portal/LoginPortal.page.js
@@ -1,26 +1,22 @@
-import {urlPortal} from '../../../support/constants'
+import {urlPortal} from '../../../support/constants';
 
 export class LoginPortalPage {
 
     goToLogin(siteDomain) {
         switch (siteDomain) {
             case 'rc-':
+            case 'test-':
                 cy.visit(`rc-${urlPortal}`)
                 break;
             case 'prod':
                 cy.visit(`${urlPortal}`)
-                break;
-            case 'test-':
-                cy.visit(`test-${urlPortal}`)
-                break;
-            default:
-                alert('We hope that this page looks ok!');
+                break
         }
     };
 
     signIn() {
-        cy.get('[type="email"]').type('seamlesspaytest@gmail.com')
-        cy.get('[type="password"]').type('123123@')
-        cy.get('[type="button"]').click()
-    }
+        cy.get('[type="email"]').type('seamlesspaytest@gmail.com');
+        cy.get('[type="password"]').type('123123@');
+        cy.get('[type="button"]').click();
+    };
 }
